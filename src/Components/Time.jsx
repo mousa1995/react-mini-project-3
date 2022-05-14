32a208bb3e10f92b87e1b92b3b7a476e139153a5
mousa1995 <mousasoccerlove@gmail.com>
2022-05-14 05:34:21 +0430

    learn how to clear using componentwillunmount

--- src/Components/Time.jsx
+++ src/Components/Time.jsx
@@ -10,11 +10,16 @@ export default class Time extends Component {
     }
 
     componentDidMount() {
-        setInterval(() => {
+        this.timer = setInterval(() => {
             this.setState({
                 Time: new Date().toLocaleTimeString()
             })
-        })
+            console.log(this.state.Time);
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer)
     }
     render() {
         return (
